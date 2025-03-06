25d2ecfa02334f2c76e0b143bd8014634eaa6ee3
kartikeywariyal <kartikeywariyal706@gmail.com>
2025-03-06 13:49:07 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import "./index.css";
-import Home from './Pages/Home'
-import Menu from './Pages/Menu'
-import Cart from './Pages/Cart'
-import Navbar from './Components/Navbar'
+import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
+import Cart from "./Pages/Cart";
+import Navbar from "./Components/Navbar";
 
 function App() {
+  const [cart, setCart] = useState([
+    { name: "Burger", price: 100, quantity: 1, image: "/images/burger.jpg" },
+    { name: "Pizza", price: 700, quantity: 2, image: "/images/pizza.jpg" }
+  ]);
 
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Cart/>
-      <Menu/>
+      <Navbar />
+      <Home />
+      <Cart cart={cart} setCart={setCart} /> {}
+      <Menu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
