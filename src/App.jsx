6ea5e0a6829ba7eb6fe7db190cd212dc02e2b031
Suraj-Sharma001 <surajsharma60923@gmail.com>
2025-03-06 14:10:54 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
+import { useState } from "react";
 import Home from './Pages/Home'
 import Navbar from './Components/Navbar'
 import Menu from "./Pages/Menu";
 import Cart from "./Pages/Cart";
 
 function App() {
-
+  
+  const [cart, setCart] = useState([
+    { name: "Burger", price: 100, quantity: 1, image: "/images/burger.jpg" },
+    { name: "Pizza", price: 700, quantity: 2, image: "/images/pizza.jpg" }
+  ]);
+  
   return (
     <>
     <Router>
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart}/> }/>
         <Route path="/menu" element={<Menu/>}/>
       </Routes>
       </Router>
